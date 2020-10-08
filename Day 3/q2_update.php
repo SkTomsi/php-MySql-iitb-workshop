91e79db9ebb5042ae0adb46a781419242f933386
Tomcy Thomas <61344052+TomcyT@users.noreply.github.com>
2020-10-08 22:27:03 +0530

    Update q2_update.php

--- Day 3/q2_update.php
+++ Day 3/q2_update.php
@@ -1,12 +1,10 @@
 <?php
-
 require("connect.php");
 
 $extract= (" SELECT * FROM `class1` WHERE name='Rohan' ");
+$getdata = mysqli_query($conn , $extract);
 
-mysqli_query($conn , $extract);
-
-while($row = mysqli_fetch_assoc($extract)){ 
+while($row = mysqli_fetch_assoc($getdata)){ 
   $name=$row['name'];
   $m5=$row['sub5'];
   $total_obt=$row['total obtained'] ;
@@ -18,7 +16,7 @@ while($row = mysqli_fetch_assoc($extract)){
   echo  "Marksheet before updating :<br> Subject 5 = ".$m5."<br>Total Marks obtained : ".$total_obt."<br>Percentage Obtained : ".$percent." %<br>";
 
   $update="UPDATE `class1` SET `sub5`=$new_m5, `total obtained`=$new_total_obt, `percent`=$new_percent WHERE name='Rohan'";
-    if (mysqli_query($connect, $update))
+    if (mysqli_query($conn, $update))
     {
       echo "<br>Update done sucessfully<br><br>";
     }
